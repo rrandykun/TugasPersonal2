6c91bc9ee9059c7f92acc630a2dfccef81c830aa
azkydinn <azky.dinn@gmail.com>
2020-08-08 09:10:58 +0700

    Menambahkan fitur login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+*/
+
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
